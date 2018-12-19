3c13b357bdbe5e45608b1c9cfc7303153581e41d
Tobias <tobias@raqt.com>
2018-12-19 11:17:45 +0100

    Moving away from windows forms.

---
+++ ConsoleRunner/Program.cs
@@ -0,0 +1,30 @@
+﻿using PackageMagic.Nuget;
+using PackageMagic.PackageManager;
+using PackagesNpm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Runner().GetAwaiter().GetResult();
+        }
+
+         static async Task Runner()
+        {
+            List<Task> t = new List<Task>();
+            var _projectDirectory = @"c:\git\tobias";
+            //clsPackages.Callback += CallbackFromPackage;
+            t.Add(clsNuget.SearchForPackagesConfig(_projectDirectory));
+            t.Add(clsNuget.PopulatePackageReferences(_projectDirectory));
+            t.Add(clsNpm.LoopPackageJson(@"C:\git\tobias\package.json"));
+            await Task.WhenAll(t);
+        }
+    }
+}
