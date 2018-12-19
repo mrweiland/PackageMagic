d5e02d9612e61cdee27fc32309c3a56e4a3753f8
SURFACEBOOK2\Lennart <lennart.a.jansson@outlook.com>
2018-12-19 22:25:39 +0100

    Just added some comments/suggestions around project files in IMagicProject.cs

--- PackageManager/Interface/IMagicProject.cs
+++ PackageManager/Interface/IMagicProject.cs
@@ -6,7 +6,15 @@ namespace PackageMagic.PackageService.Interface
     public interface IMagicProject
     {
         string Name { get; set; }
+
         string Path { get; set; }
+
+        //TODO! Could be nice to have this value, maybe have a separate csproj-parser 
+        //that could lift out some basic information about the project? 
+        //In that case we could start already at project level to read the xml into a stream
+        //and then feed the Nuget class with that stream so it could continue to parse references
+        string FrameworkVersion { get; set; }
+
         List<IMagicPackage> Packages { get; set; }
     }
 }
