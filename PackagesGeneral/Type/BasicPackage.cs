c291934299213a22d79b707bba61a259a0ba05b6
SURFACEBOOK2\Lennart <lennart.a.jansson@outlook.com>
2018-12-21 11:45:51 +0100

    Implemented the new CsProj parser

---
+++ PackagesGeneral/Type/BasicPackage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PackageMagic.General.Interface;
+
+namespace PackageMagic.General.Type
+{
+    public class BasicPackage : IMagicPackage
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public static List<IMagicPackage> PackageInformation = new List<IMagicPackage>();
+        public string Description { get; set; }
+        public MagicPackageType PackageType { get; set; }
+    }
+}
